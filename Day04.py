968e50020ea4e534c120abdc092e27cf19935b26
Natarajan Rodrigues <natarajanrodrigues@gmail.com>
2021-01-24 22:49:32 -0300

    day 04 part1 and 2

--- Day04.py
+++ Day04.py
@@ -15,8 +15,27 @@ with open("input-day04.txt", "r") as file:
 # with open("input-day04-example.txt", "r") as file:
   entries = [[x for x in re.split(":\S+ ?", line.replace("\n", " "))] for line in file.read().split("\n\n")]  
 
+with open("input-day04.txt", "r") as file:
+  entries2 = [line.replace("\n", " ") for line in file.read().split("\n\n")]  
+
+def valid_passport(passport): 
+  if ( 
+    re.search(r"byr:19[2-9][0-9]|byr:200[0-2]", passport) and
+    re.search(r"iyr:20[1][0-9]|iyr:2020", passport) and
+    re.search(r"eyr:202[0-9]|eyr:2030", passport) and
+    re.search(r"hgt:1([5-8][0-9]|9[0-3])cm|hgt:(59|6[0-9]|7[0-6])in", passport) and
+    re.search(r"hcl:#[a-f0-9]{6}", passport) and
+    re.search(r"ecl:(amb|blu|brn|gry|grn|hzl|oth)", passport) and
+    re.search(r"pid:\d{9}\b", passport) # that final "\b" prevents pid's with more than 9 digits example: 1234567890 is invalid. Without the "\b" the re catch it as valid because it has at least 9 digits
+        # but what we want is exactaly 9 digits and nothing more at all. 
+    ):
+    return True
+  else: 
+    return False
+
 
 # Part1
 print("Result 1: ", len(list(filter(lambda x: all(elem in x for elem in mandatory_fields), copy.deepcopy(entries) ))) )
 
 # Part2
+print("Result 2: ", len(list(filter(lambda x: valid_passport(x), entries2 ))) )
--- teste.py
+++ teste.py
@@ -1,11 +1,25 @@
 
+import re
 
-# List of string 
-list1 = ['ecl', 'pid', 'eyr', 'hcl', 'byr', 'iyr', 'cid', 'hgt', '']
-# List of string
-list2 = ['ecl', 'pid', 'eyr', 'hcl', 'byr', 'iyr', 'cid', 'hgt']
-result =  all(elem in list1  for elem in list2)
-if result:
-    print("Yes, list1 contains all elements in list2")    
-else :
-    print("No, list1 does not contains all elements in list2")
+# ['ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm', 'iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884 hcl:#cfa07d byr:1929', 'hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm', 'hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in ']
+
+# re.search(r"byr:19[2-9][0-9]|byr:200[0-2]", str)
+# re.search(r"iyr:20[1][0-9]|iyr:2020", str)
+# re.search(r"eyr:20[2][0-9]|eyr:2030", str)
+# re.search(r"hgt:1([5-8][0-9]|9[0-3])cm|hgt:(59|6[0-9]|7[0-6])in", str)
+# re.search(r"hgt:1([5-8][0-9]|9[0-3])cm|hgt:(59|6[0-9]|7[0-6])in", str)
+# re.search(r"hcl:#([a-f0-9]{6})", str)
+# re.search(r"ecl:(amb|blu|brn|gry|grn|hzl|oth)", str)
+# re.search(r"pid:[0-9]{9}", str)
+
+str = 'ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm'
+if ( re.search(r"hgt:1([5-8][0-9]|9[0-3])cm|hgt:(59|6[0-9]|7[0-6])in", str) and
+    re.search(r"iyr:20[1][0-9]|iyr:2020", str) and
+    re.search(r"eyr:20[2][0-9]|eyr:2030", str) and
+    re.search(r"hgt:1([5-8][0-9]|9[0-3])cm|hgt:(59|6[0-9]|7[0-6])in", str) and
+    re.search(r"hgt:1([5-8][0-9]|9[0-3])cm|hgt:(59|6[0-9]|7[0-6])in", str) and
+    re.search(r"hcl:#([a-f0-9]{6})", str) and
+    re.search(r"ecl:(amb|blu|brn|gry|grn|hzl|oth)", str) and
+    re.search(r"pid:[0-9]{9}", str) 
+    ):
+    
