b49b7efe3840134d9a13693469d8b42e23ab9c1a
Natarajan Rodrigues <natarajanrodrigues@gmail.com>
2021-01-28 16:58:29 -0300

    Day 7 part2

--- Day07.py
+++ Day07.py
@@ -34,5 +34,37 @@ def part_1(entries):
       count += 1
   return count
 
-print("Result 1: ", part_1(entries))
   
+print("Result 1: ", part_1(entries))
+
+def get_total_num_bags(color_bag):
+  
+  if color_bag not in map_bags.keys():
+    return 0
+  if color_bag in map_bags.keys() and map_bags[color_bag] == 0:
+    return 1
+  count = 0
+  for sub_color in map_bags[color_bag].keys():
+    
+    count_sub_bags = get_total_num_bags(sub_color)
+    count += map_bags[color_bag][sub_color] + map_bags[color_bag][sub_color] * count_sub_bags
+  return count
+
+def part_2(entries):
+  for line in entries:
+    split_result = line.split(" contain ")
+    bag = split_result[0].replace(" bags", "").replace(" bag", "")
+    contents = split_result[1].replace(" bags", "").replace(" bag", "").split(", ")
+    # map_bags[bag] = [re.sub(r"^\d+ ", "", item) for item in contents]
+    sub_colors = {}
+    for color in contents:
+      number = re.findall("\d+", color)
+      sub_color = re.sub(r"^\d+ ", "", color)
+      if (len(number) > 0):
+        sub_colors[sub_color] = int(number[0])
+    map_bags[bag] = sub_colors
+
+  return get_total_num_bags("shiny gold")
+
+  
+print("Result 2: ", part_2(entries))
