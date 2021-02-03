7fe9dac9255a66cc80bafc4e5564e737857bf6ff
Natarajan Rodrigues <natarajanrodrigues@gmail.com>
2021-02-02 21:46:52 -0300

    Day 17 part 1 - learning to use tuples

--- teste.py
+++ teste.py
@@ -27,14 +27,69 @@ pocket = set()
 
 dimensions = 3
 
-with open("input-day17.txt") as initf:
-	for y, line in enumerate(initf.readlines()):
-		for x, activity in enumerate(line.strip()):
-			if activity == '#':
-				print(x, " ", y)
-				# Add the right amount of dimensions
-				cube_pos = (x,y) + (0,)*(dimensions-2)
-				pocket.add(cube_pos)
+# with open("input-day17.txt") as initf:
+# with open("input-day17-example.txt") as initf:
+# 	for y, line in enumerate(initf.readlines()):
+# 		for x, activity in enumerate(line.strip()):
+# 			if activity == '#':
+# 				print(x, " ", y)
+# 				# Add the right amount of dimensions
+# 				cube_pos = (x,y) + (0,)*(dimensions-2)
+# 				pocket.add(cube_pos)
 
-print(pocket)
-print(len(pocket))
+# print(pocket)
+# new = {(1,1,1)}
+# pocket = new
+# print(pocket)
+# print(len(pocket))
+
+
+
+
+def p1(active):
+    for _ in range(6):
+        new = set()
+        rem = set()
+        xvals = [x[0] for x in active]
+        yvals = [y[1] for y in active]
+        zvals = [z[2] for z in active]
+
+        for x in range(min(xvals) - 1, max(xvals) + 2):
+            for y in range(min(yvals) - 1, max(yvals) + 2):
+                for z in range(min(zvals) - 1, max(zvals) + 2):
+                    nbrs = 0
+                    for dx in [-1, 0, 1]:
+                        for dy in [-1, 0, 1]:
+                            for dz in [-1, 0, 1]:
+                                if dx != 0 or dy != 0 or dz != 0:
+                                    if (x + dx, y + dy, z + dz) in active:
+                                        nbrs += 1
+
+                    if (x, y, z) not in active and nbrs == 3:
+                        new.add((x, y, z))
+                        # active.add((x, y, z))
+                    if (x, y, z) in active and (nbrs < 2 or nbrs > 3): 
+                        rem.add((x, y, z))
+                    if (x, y, z) in active and nbrs in [2, 3]:
+                        new.add((x, y, z))
+                        # active.add((x, y, z))
+
+        # for i in rem:
+        #     active.remove(i)
+        active = active.difference(rem)
+        active = active.union(new)
+        # active = new 
+
+    return len(active)
+
+
+
+data = open("input-day17-example.txt").read().strip().split('\n')
+
+active = set()
+for r, l in enumerate(data):
+    for c, p in enumerate(l):
+        if p == '#':
+            active.add((r, c, 0))
+
+print(f'Part 1: {p1(active)}')
