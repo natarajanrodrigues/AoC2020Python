af20a59cb05b2f9760d0dd9cc195fcf5dbb004c7
Natarajan Rodrigues <natarajanrodrigues@gmail.com>
2021-02-05 15:08:37 -0300

    Day 16 part 1 and 2

--- Day16.py
+++ Day16.py
@@ -3,7 +3,8 @@ with open("input-day16.txt", "r") as file:
   entries = file.read().split("\n\n")
 
 rules_entries = entries[0].split("\n")
-your_ticker = entries[1].split("\n")[1]
+your_ticket = entries[1].split("\n")[1]
+
 # nearby_tickets = entries[2].split("nearby tickets:\n")[1].split("\n")[:-1]
 nearby_tickets = entries[2].split("nearby tickets:\n")[1].split("\n")
 rules = {}
@@ -18,7 +19,11 @@ for value in rules.values():
     v = i.split("-")
     for num in range(int(v[0]),int(v[1]) + 1):
       valid_values.add(num)
-        
+
+possibles = {}
+for i in rules.keys():
+  possibles[i] = list(range(0,20))
+      
 
 def part_1(): 
   invalid = []
@@ -42,19 +47,90 @@ def is_valid(ticket):
       return False
   return True
 
+
+def found_departures():
+  departure_keys = filter(lambda rule: rule.startswith("departure") and len(possibles[rule]) == 1 , possibles.keys())
+  return True if len(list(departure_keys)) == 6 else False
+
+def all_index_pass(index, range1, range2, valid_tickets):
+  for ticket in valid_tickets:
+    splited_ticket = ticket.split(",")
+    num = int(splited_ticket[index])
+    if num not in range1 and num not in range2:
+      return False
+  return True
+
+def verify_rule(rule, valid_tickets):
+  this_rules = rules[rule]
+  range1a = int( this_rules[0].split("-")[0])
+  range1b = int( this_rules[0].split("-")[1])
+  
+  range2a = int( this_rules[1].split("-")[0])
+  range2b = int( this_rules[1].split("-")[1])
+  
+  range1 = range(range1a, range1b + 1)
+  range2 = range(range2a, range2b + 1)
+
+  invalid_tickets_index = []
+  
+  for index in possibles[rule]:
+    if not all_index_pass(index, range1, range2, valid_tickets):
+      invalid_tickets_index.append(index)
+  
+  possibles_rules = possibles[rule]
+  for i in invalid_tickets_index:
+    possibles_rules.remove(i)
+  possibles[rule] = possibles_rules
+    
+
+  
+
+  # range2 = range(int(this_rules[1].split("-")[0]), int(this_rules[1].split("-")[1]])
+  # print(range1b)
+
+
 def part_2(): 
   valid_tickets = []
   for ticket in nearby_tickets:
     if is_valid(ticket):
       valid_tickets.append(ticket)
   
-  valid_tickets.append(your_ticker)
+  valid_tickets.append(your_ticket)
+
+  verify_rule('arrival location', valid_tickets)
+
+  for rule in possibles:
+    verify_rule(rule, valid_tickets) # verifying the possibles valid ticket index for each rule
+
+  # for i in possibles.keys():
+  #   print(i, " ", possibles[i])
+  while found_departures() == False: 
+    founded_keys = list(filter(lambda rule: len(possibles[rule]) == 1 , possibles.keys()))
+    
+    for key in founded_keys:
+      right_index = possibles[key][0]
+      for i in possibles.keys():
+        if i != key and right_index in possibles[i]:
+          possibles[i].remove(right_index)
+
+
+  departures_keys = list(filter(lambda rule: rule.startswith("departure") and len(possibles[rule]) == 1 , possibles.keys()))
+  result = 1
+  
+  splitted_ticket = [int(i) for i in your_ticket.split(",")]
+  
+  for key in departures_keys:
+    index = possibles[key][0]
+    result *= int(splitted_ticket[index])
+
+  return result
+
+  
+
   
 
 
-  
 
 
-
-# print("Part 2: ", part_2())
+print("Part 2: ", part_2())
 
