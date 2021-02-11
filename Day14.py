85a3073aac3785fa744b097be17c3825a7dd6697
Natarajan Rodrigues <natarajanrodrigues@gmail.com>
2021-02-11 11:56:22 -0300

    day 14 part 2

--- Day14.py
+++ Day14.py
@@ -1,6 +1,6 @@
 import re
 
-def parse_mask(number, mask):
+def parse_address(number, mask):
   bin_number = format(int(number), "036b")
   result = ""
   for i,j in zip(bin_number, mask):
@@ -11,6 +11,7 @@ def parse_mask(number, mask):
   return (int(result, 2))
   
 with open("input-day14.txt") as file:
+# with open("input-day14-example.txt") as file:
   entries = [line[:-1] for line in file.readlines()]
 
 def part_01(lines):
@@ -19,10 +20,55 @@ def part_01(lines):
     if line.startswith("mask"):
       mask = line.split(" = ")[1]
     else: 
-      split_line = line.split(" = ")
-      index = re.findall(r"[0-9]+", split_line[0])[0]
-      mem[index] = parse_mask(split_line[1], mask)
+      index, unparsed_number = line.split(" = ")
+      index = re.findall(r"[0-9]+", index)[0]
+      mem[index] = parse_address(unparsed_number, mask)
 
   return sum(mem.values())
 
 print("Part 1: ", part_01(entries)) 
+
+def generate_addresses(mask):
+  where_is_x = mask.find("X")
+  if where_is_x == -1:
+    return [mask]
+  mask_0 = mask[:where_is_x] + '0' + mask[where_is_x + 1:]
+  mask_1 = mask[:where_is_x] + '1' + mask[where_is_x + 1:]
+  
+  # without '*'
+  # [generate_addresses(mask_0), generate_addresses(mask_1)] ==> [['100'], ['101']], [['110'], ['111']]]
+
+  # with '*' => something like a flat map
+  # [*generate_addresses(mask_0), *generate_addresses(mask_1)] ==> ['100', '101', '110', '111']
+  return [*generate_addresses(mask_0), *generate_addresses(mask_1)]
+
+
+def parse_mask(number, mask):
+  bin_number = format(int(number), "036b")
+  result = ""
+  for i,j in zip(bin_number, mask):
+    if j == 'X':
+      result += 'X'
+    else:
+      result += str(int(i) | int(j))
+  return result
+
+def part_02(lines):
+  mem = {}
+  for line in lines:
+    if line.startswith("mask"):
+      mask = line.split(" = ")[1]
+    else: 
+      index, unparsed_number = line.split(" = ")
+      index = re.findall(r"[0-9]+", index)[0]
+      
+      final_mask = parse_mask(index, mask)
+            
+      addresses = generate_addresses(final_mask)
+      for i in addresses: 
+        mem[int(i, 2)] = int(unparsed_number)
+
+  return sum(mem.values())
+
+print("Part 2: ", part_02(entries)) 
+
