33b8971186092a87a74b450a0c18d28a633fac6c
Natarajan Rodrigues <natarajanrodrigues@gmail.com>
2021-02-24 18:31:24 -0300

    Finished! Day 20 part 2

--- Day20.py
+++ Day20.py
@@ -2,8 +2,8 @@ import re
 import math 
 from itertools import combinations, product
 
-with open("input-day20-example.txt") as file:
-# with open("input-day20.txt") as file:
+# with open("input-day20-example.txt") as file:
+with open("input-day20.txt") as file:
   entries = file.read().split("\n\n")
 
 tiles = {}
@@ -86,7 +86,6 @@ def print_matrix(matrix, tam):
     print()
   print()
 
-
 def order_tile(base_tile, unordered_tile):
   tile_1 = base_tile
   tile_2 = unordered_tile
@@ -101,9 +100,7 @@ def order_tile(base_tile, unordered_tile):
     if match_tiles(tile_1, tile_2) != -1 :
       return tile_2
   return None
-
   
-
 def order_tile_to_position(base_tile, unordered_tile, position):
 
   # tile_1 = base_tile
@@ -112,6 +109,8 @@ def order_tile_to_position(base_tile, unordered_tile, position):
     unordered_tile = transform_tile(e, unordered_tile)
     if match_tiles(base_tile, unordered_tile) == position:
       return unordered_tile
+
+
 
 def part_1():
 
@@ -132,7 +131,7 @@ def part_1():
       neighbors[number_2] = n2
     
       
-  print(len(neighbors.keys()))
+  # print(len(neighbors.keys()))
   
   product = 1
   p = []
@@ -155,9 +154,10 @@ def generate_img(number_matrix, tam):
       list_line.append((x,y))
     list_line = list(map(lambda x: number_matrix[x], list_line))
     
-    print(list_line)
+    # print(list_line)
     for k in range(1,9):
       line = ''
+      # for index in range(0,len(list_line)) :
       for index in range(0,len(list_line)) :
         number = list_line[index]
         t = tiles[number]
@@ -184,68 +184,15 @@ def print_content_matrix(tiles, number_matrix, tam):
         print(t[k][0:10], end=" ")
       print()
     print()
+
+def print_img(matrix):
+  print()
+  print("Printing image")
+
+  for x in range(0, len(matrix)):
+    print(matrix[x])
+  print()
   
-
-
-# def order_matrix(tiles, number_matrix, tam):
-#   # take 0,0 and 0,1 and put them on right positions
-#   tile_1 = tiles[number_matrix[0,0]]
-#   tile_2 = tiles[number_matrix[0,1]]
-#   tile_3 = tiles[number_matrix[1,0]]
-  
-#   print(number_matrix[0,1])
-#   print(number_matrix[1,0])
-
-#   for e in range(0, 30):
-#     if e > 0 and e + 1 % 5 == 0:
-#       tile_2 = flip_h(tile_2) 
-#     if e + 1 == 10:
-#       tile_2 = flip_v(tile_2)
-#     if e > 0 and e + 1 % 4 != 1:
-#       tile_2 = rotate(tile_2)
-
-#     if e == 15:
-#       tile_1 = tiles[number_matrix[0,0]]
-#       tile_2 = tiles[number_matrix[0,1]]
-  
-#     if e > 14 and e + 1 % 5 == 0:
-#       tile_1 = flip_h(tile_1) 
-#     if e > 14 and e + 1 == 24:
-#       tile_1 = flip_v(tile_1)
-#     if e > 14 and e + 1 % 4 != 1:
-#       tile_1 = rotate(tile_1)
-  
-#     if match_tiles(tile_1, tile_2) == 3 :
-#       # print_single_tile(number_matrix[0,0], tile_1)
-#       # print_single_tile(number_matrix[0, 1], tile_2)
-
-#       tiles[number_matrix[0,0]] = tile_1
-#       tiles[number_matrix[0,1]] = tile_2
-#       break
-
-#   if match_tiles(tile_1, tile_3) == 4:
-#     tile_1 = flip_h(tile_1)
-#     tile_2 = flip_h(tile_2)
-
-
-#   # print_single_tile(number_matrix[0,0], tiles[number_matrix[0,0]])
-#   # print_single_tile(number_matrix[0,1], tiles[number_matrix[0,1]])
-#   for coordenate in (x for x in product(range(0, tam), repeat=2) if x != (0,0)) :
-#     # print(coordenate, coordenate[1])
-#     if coordenate[1] == 0 and coordenate != (0,0):
-#       num_1 = (coordenate[0] - 1, coordenate[1])  
-#     elif coordenate[1] > 0:
-#       num_1 = (coordenate[0], coordenate[1] - 1)
-    
-#     num_2 = coordenate
-#     tile_1 = tiles[number_matrix[num_1]]
-#     # print(coordenate)
-#     tile_2 = order_tile(tile_1, tiles[number_matrix[num_2]]) 
-#     tiles[coordenate] = tile_2
-#     # print_single_tile(number_matrix[0,0], tiles[number_matrix[0,0]])
-#     # print_single_tile(number_matrix[0,1], tiles[number_matrix[0,1]])
-
-
 def transform_tile(k, tile):
   # if k % 4 == 0:
   #   return tile
@@ -256,23 +203,25 @@ def transform_tile(k, tile):
     tile = flip_v(tile)
   
   return tile
-   
 
+                  # 
+#    ##    ##    ###
+ #  #  #  #  #  #   
 def order_matrix(tiles, number_matrix, tam):
   # take 0,0 and 0,1 and put them on right positions
   tile_1 = tiles[number_matrix[0,0]]
   tile_2 = tiles[number_matrix[0,1]]
   tile_3 = tiles[number_matrix[1,0]]
   
-  print(number_matrix[0,1])
-  print(number_matrix[1,0])
+  # print(number_matrix[0,1])
+  # print(number_matrix[1,0])
 
   e = 0
   tile_1 = tiles[number_matrix[0,0]]
   tile_2 = tiles[number_matrix[0,1]]
 
   # asdfasfd
-  print_content_matrix(tiles, number_matrix, tam)
+  # print_content_matrix(tiles, number_matrix, tam)
 
   # put 0,0 and 0,1 on position
   e = 0
@@ -315,41 +264,85 @@ def order_matrix(tiles, number_matrix, tam):
     e += 1
     tile_3 = transform_tile(e, tile_3)
     
-  print_content_matrix(tiles, number_matrix, tam)
+  # print_content_matrix(tiles, number_matrix, tam)
 
-  # print_single_tile(number_matrix[0,0], tiles[number_matrix[0,0]])
-  # print_single_tile(number_matrix[0,1], tiles[number_matrix[0,1]])
   for point in (x for x in product(range(0, tam), repeat=2) if x != (0,0)) :
-    # print(point, point[1])]
-
-    # STUCK HERE    
-    if point[1] == 0 and point != (0,0):
-      num_1 = (point[0] - 1, point[1])  
+    
+    if point[1] == 0 :
+      num_ref = (point[0] - 1, point[1])  
     elif point[1] > 0:
-      num_1 = (point[0], point[1] - 1)
+      num_ref = (point[0], point[1] - 1)
     
-    num_2 = point
-    
-    print(num_1)
-    print(point)
+    if point == (0,0) or num_ref == (0,0):
+      continue
 
-    tile_1 = tiles[number_matrix[num_1]]
-    # print(point)
+    tile_1 = tiles[number_matrix[num_ref]]
+    
     if point[1] == 0 and point != (0,0):
-      tile_2 = order_tile_to_position(tile_1, tiles[number_matrix[num_2]], 2) 
+      tile_2 = order_tile_to_position(tile_1, tiles[number_matrix[point]], 2) 
     elif point[1] > 0:
-      tile_2 = order_tile_to_position(tile_1, tiles[number_matrix[num_2]], 3) 
-    tiles[point] = tile_2
-    
-    
-    # print_content_matrix(tiles, number_matrix, tam)
-    # print_single_tile(number_matrix[0,0], tiles[number_matrix[0,0]])
-    # print_single_tile(number_matrix[0,1], tiles[number_matrix[0,1]])
+      tile_2 = order_tile_to_position(tile_1, tiles[number_matrix[point]], 3) 
+    tiles[number_matrix[point]] = tile_2
+
+def count_monsters(image_matrix):
+  monster_points = [(0,18), 
+    (1,0), (1,5), (1,6), (1,11), (1,12), (1,17), (1,18), (1,19),   
+    (2,1), (2,4), (2,7), (2,10), (2,13), (2,16)
+    ]
+
+  num_columns = len(image_matrix[0])
+  monsters = 0
+  for line in range(0, len(image_matrix) - 3):
+    for column in range(0,num_columns - 20):
+      count = 0
+      points = set()
+      for monster_point in monster_points:
+        image_point = image_matrix[line + monster_point[0]][column + monster_point[1]]
+        points.add(image_point)
+        if image_point == '#' or image_point == '0':
+          count += 1
+      if count == 15:
+        monsters += 1
+  return monsters
+  
+def roughness(image_matrix):
+  
+  monster_points = [(0,18), 
+    (1,0), (1,5), (1,6), (1,11), (1,12), (1,17), (1,18), (1,19),   
+    (2,1), (2,4), (2,7), (2,10), (2,13), (2,16)
+    ]
+
+  num_columns = len(image_matrix[0])
+  for line in range(0, len(image_matrix) - 3):
+    for column in range(0,num_columns - 20):
+      count = 0
+      points = set()
+      for monster_point in monster_points:
+        image_point = image_matrix[line + monster_point[0]][column + monster_point[1]]
+        points.add((line + monster_point[0], column + monster_point[1]))
+        if image_point == '#' or image_point == 'O':
+          count += 1
+      if count == 15:
+        
+        for point in points:
+          # image_point = image_matrix[line + monster_point[0]][column + monster_point[1]]
+          a_line = image_matrix[point[0]]
+          image_matrix[point[0]] = a_line[0:point[1]] + '0' + a_line[point[1] + 1:] 
+  
+  num_column = len(image_matrix[0])
   
 
+  roughness = 0
+  for line in range(0, len(image_matrix)):
+    for char in image_matrix[line]:
+      if char == '#':
+        roughness += 1
+
+  return roughness
 
 
 
+# PART 2
 def part_2():
 
   tam = int(math.sqrt(len(tiles.keys())))
@@ -358,8 +351,6 @@ def part_2():
   number_matrix = {}
   number_matrix[0,0] = corner_0
   ns = [ i for i in neighbors[corner_0] ]
-  # number_matrix[1,0] = ns[0]
-  # number_matrix[0,1] = ns[1]
   number_matrix[1,0] = ns[1]
   number_matrix[0,1] = ns[0]
 
@@ -373,79 +364,35 @@ def part_2():
           n.append((i - 1, j))
           n.append((i + 1, j))
           n = list(filter(lambda x: x in number_matrix.keys(), n))
-          inter = neighbors[number_matrix[n[0]]]
-          for x in range(1,len(n)):
-            set2 = neighbors[number_matrix[n[x]]]
-            inter = inter.intersection(set2)
-          res_mapped_keys = set(list(map(lambda k: number_matrix[k], number_matrix.keys())))
-          inter = inter.difference(res_mapped_keys)
-          if len(inter) == 1:
-            number_matrix[i,j] = next(iter(inter))
+          if n != []:
+            inter = neighbors[number_matrix[n[0]]]
+            for x in range(1,len(n)):
+              set2 = neighbors[number_matrix[n[x]]]
+              inter = inter.intersection(set2)
+            res_mapped_keys = set(list(map(lambda k: number_matrix[k], number_matrix.keys())))
+            inter = inter.difference(res_mapped_keys)
+            if len(inter) == 1:
+              number_matrix[i,j] = next(iter(inter))
 
-  print_matrix(number_matrix, tam)
-
-  # print_single_tile('2311', tiles['2311'])
-  # print_single_tile('1951', tiles['1951'])
-  # print_single_tile('2729', tiles['2729'])
-  # print_single_tile('2311', tiles['2311'])
-
-
-  print("ordering matrix")
-  new_tiles = order_matrix(tiles,number_matrix, tam)
+  # print_matrix(number_matrix, tam)
   
-  # print_single_tile('1951', tiles['1951'])
-  # print_single_tile('2729', tiles['2729'])
-  # print_single_tile('2311', tiles['2311'])
+  # ordering matrix
+  order_matrix(tiles, number_matrix, tam)
+  # print_content_matrix(tiles, number_matrix, tam)
   
+  matrix_image = generate_img(number_matrix, tam)
   
+  # print_img(matrix_image)
 
-  # matrix_image = generate_img(number_matrix, tam)
-  # print(matrix_image)
+  for i in range(0,8):
+    if i == 4: 
+      matrix_image = flip_v(matrix_image)
 
-  print_content_matrix(tiles, number_matrix, tam)
+    if count_monsters(matrix_image) == 0:
+      matrix_image = rotate(matrix_image)
+    else:
+      break
 
-  
-
-  # print("AFFAFAFA")
-  # tile = tiles['1951']
-  # for i in range(0,25):
-  #   print("Transformation", i)
-  #   tile = transform_tile(i, tile)
-  #   print_single_tile('1951', tile)
-
-  # tile_1 = tiles['1951']
-  # tile_2 = tiles['2729']
-
-  # e = 0
-  # k = 0
-
-  # print_single_tile('1951', tile_1)
-
-  # while True:  
-  #   if e == 9:
-  #     k += 1
-  #     tile_1 = transform_tile(k, tile_1)
-  #     e = 0
-  #     tile_2 = tiles['2729']
-
-  #   print_single_tile('2729', tile_2)
-  #   tile_2 = transform_tile(e, tile_2)
-
-  #   if match_tiles(tile_1, tile_2) == 3 :
-  #     print_single_tile('1951', tile_1)
-  #     print_single_tile('2729', tile_2)
-      
-  #     break
-  #   e += 1
-  # print('finish')
-
+  return roughness(matrix_image)
 
 print("Part 2: ", part_2())
-
-
-  
-
-
-
-      
-
