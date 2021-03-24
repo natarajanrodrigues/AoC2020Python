823f74ce4dcef5da0350d728bc237c9847cc4acf
Natarajan Rodrigues <natarajanrodrigues@gmail.com>
2021-03-24 17:39:36 -0300

    Day 23 - part 2

--- Day23-part2.py
+++ Day23-part2.py
@@ -1,3 +1,5 @@
+import time
+
 class Node: 
   def __init__(self, value):
     self.value = value
@@ -33,8 +35,12 @@ class LinkedList:
     node_prev.next = node_next
     node_next.previous = node_prev
     
+    if node_to_remove is self.head:
+      self.head = self.head.next
+
     node_to_remove.next = None
     node_to_remove.previous = None
+
 
     self.nodes_map.pop(value)
 
@@ -60,80 +66,6 @@ class LinkedList:
       print()
       return
 
-  def destination(self, pick_up):
-    values = [ i for i in self.nodes_map.keys()]
-    values.sort()
-    for i in pick_up:
-      values.remove(i)
-
-    crab_cursor_index = values.index(self.crab_cursor.value)
-    if crab_cursor_index  > 0:
-      return values[crab_cursor_index - 1]
-    else:
-      return values[len(values) - 1]
-  
-  def move(self, num_move):
-    # print("\n-- move {0}--".format(num_move))
-    # print("cups: ", end="")
-    # self.print_cups()
-    
-    print(num_move, end = "\r")
-
-    pick_cursor_init = self.crab_cursor.next
-    pick_cursor_end = self.crab_cursor
-    pick_up = []
-    
-    for i in range(0,3):      
-      pick_cursor_end = pick_cursor_end.next
-      pick_up.append(pick_cursor_end.value)
-      
-
-    self.crab_cursor.next = pick_cursor_end.next
-    self.crab_cursor.next.previous = self.crab_cursor
-
-    pick_cursor_init.previous = None
-    pick_cursor_end.next = None
-    
-    
-    # print("pick up: ", ' '.join([str(e) for e in pick_up]))
-    
-    
-    # destination = self.destination(pick_up)
-
-    destination = self.crab_cursor.value
-    while True:
-      destination -= 1
-      if destination < min:
-        destination = max
-      if destination not in pick_up:
-        break 
-    
-    # print("destination: ", destination)
-
-    # making the moves
-    node_destination = self.nodes_map[destination]
-    next_node_destination = node_destination.next
-
-    node_destination.next = pick_cursor_init
-    pick_cursor_init.previous = node_destination
-    pick_cursor_end.next = next_node_destination
-    next_node_destination.previous = pick_cursor_end
-
-    self.crab_cursor = self.crab_cursor.next
-
-
-  # def add_after(self, num_after, num_to_add):
-  #   cursor = self.head
-  #   while cursor.value != num_after:
-  #     cursor = cursor.next
-    
-  #   newNode = Node(num_to_add)
-    
-  #   next_c = cursor.next
-  #   cursor.next = newNode
-  #   newNode.previous = cursor
-  #   newNode.next = next_c
-  #   next_c.previous = newNode
 
   def add_after(self, num_after, list_to_add):
     cursor = self.nodes_map[num_after]
@@ -148,8 +80,6 @@ class LinkedList:
       next_c.previous = newNode
       self.nodes_map[i] = newNode
       cursor = cursor.next
-    
-
 
 
   def play(self, n_moves):
@@ -160,17 +90,20 @@ class LinkedList:
 
     for i in range(0, n_moves):
       # self.move(i + 1)
+      # print(i + 1, end = "\r")
 
-      print("\n-- move {0}--".format(i))
-      print("cups: ", end="")
-      self.print_cups()
+      # print("\n-- move {0}--".format(i + 1))
+      # print("cups: ", end="")
+      # self.print_cups()
       
       pick_up = []
-      pick_cursor = self.crab_cursor
+      pick_cursor = self.crab_cursor.next
       for i in range(0,3):      
         pick_up.append(pick_cursor.value)
-        self.remove(pick_cursor.value)
         pick_cursor = pick_cursor.next
+      
+      for i in range(0,3):      
+        self.remove(pick_up[i])
 
       destination = int(self.crab_cursor.value)
 
@@ -181,12 +114,13 @@ class LinkedList:
         if destination not in pick_up:
           break 
       
+      # print("pick up: ", ' '.join([str(e) for e in pick_up]))
+      # print("destination: ", destination)
+      
       self.add_after(destination, pick_up)
+      self.crab_cursor = self.crab_cursor.next    # end for
 
-      self.crab_cursor = self.crab_cursor.next
-
-
-
+    # printing final results
     # print("\n-- final --")
     # print("cups: ", end="")
     # self.print_cups()
@@ -206,7 +140,6 @@ class LinkedList:
       res.append(cursor.value)
       cursor = cursor.next
 
-    # print(res)
     return ''.join([str(e) for e in res])
 
 
@@ -216,29 +149,22 @@ class LinkedList:
 def part_1(input):
   linked_list = LinkedList()
   linked_list.add_str_array(input)
-
   linked_list.play(100)
-
   return linked_list.labels_after_1()
 
 def part_2(input):
   linked_list = LinkedList()
   linked_list.add_str_array(input)
   for i in range(10, 1_000_001):
-    linked_list.add(str(i))
-  
+    linked_list.add(i)
   linked_list.play(10_000_000)
-
-  v1 = int(linked_list.nodes_map[1].next.value)
-  v2 = int(linked_list.nodes_map[1].next.next.value)
-
-
+  v1 = linked_list.nodes_map[1].next.value
+  v2 = linked_list.nodes_map[1].next.next.value
   return v1 * v2
 
 def main():
-  print("Part 1:", part_1("643719258") )
-  # print("Part 2:", part_2("389125467") )
-
+  print("Part 1:", part_1("643719258") )  
+  print("Part 2:", part_2("643719258") )
 
 
 if __name__ == "__main__":
