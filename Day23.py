ccc4a75a514ec0acd0f8ed56899f5b3a804bdef5
Natarajan Rodrigues <natarajanrodrigues@gmail.com>
2021-03-25 19:49:14 -0300

    Day 23 part 2

--- Day23.py
+++ Day23.py
@@ -1,3 +1,5 @@
+import time
+
 class Node: 
   def __init__(self, value):
     self.value = value
@@ -5,9 +7,11 @@ class Node:
     self.previous = None
 
 class LinkedList:
+
   def __init__ (self):
     self.head = None
     self.crab_cursor = self.head
+    self.nodes_map = {}
 
   def add(self, value):
     newNode = Node(value)
@@ -17,42 +21,29 @@ class LinkedList:
       self.head.previous = newNode
       self.crab_cursor = self.head
     else:
-      
       previous_head = self.head.previous
       previous_head.next = newNode
-      
       newNode.previous = previous_head
       newNode.next = self.head
       self.head.previous = newNode
+    self.nodes_map[value] = newNode
 
   def remove(self, value):
-    cursor = self.head
-    while cursor.value is not value:
-      cursor = cursor.next
-    if cursor is self.head:
-      self.head = self.head.next
+    node_to_remove = self.nodes_map[value]
+    node_prev = node_to_remove.previous
+    node_next = node_to_remove.next
+    node_prev.next = node_next
+    node_next.previous = node_prev
     
-    c_prev = cursor.previous
-    c_next = cursor.next
-    cursor.next = None
-    cursor.previous = None
+    if node_to_remove is self.head:
+      self.head = self.head.next
 
-    c_next.previous = c_prev
-    c_prev.next = c_next
-  
-  def print(self):
-    if self.head is None:    
-      print("List is empty");    
-      return
-    else:  
-      print("Printing...")
-      cursor = self.head
-      print(cursor.value, end="")
-      while cursor.next is not self.head:
-        cursor = cursor.next
-        print(cursor.value, end="")
-      print()
-      return
+    node_to_remove.next = None
+    node_to_remove.previous = None
+
+
+    self.nodes_map.pop(value)
+
   
   def print_cups(self):
     if self.head is None:    
@@ -75,124 +66,82 @@ class LinkedList:
       print()
       return
 
-  def destination(self, pick_up):
-    values = []
-    cursor = self.head
-    values.append(cursor.value)
-    while cursor.next is not self.head:
+
+  def add_after(self, num_after, list_to_add):
+    cursor = self.nodes_map[num_after]
+    
+    for i in list_to_add:
+      newNode = Node(i)
+      
+      next_c = cursor.next
+      cursor.next = newNode
+      newNode.previous = cursor
+      newNode.next = next_c
+      next_c.previous = newNode
+      self.nodes_map[i] = newNode
       cursor = cursor.next
-      values.append(cursor.value)
-    values.sort()
-    
-    crab_cursor_index = values.index(self.crab_cursor.value)
-    if crab_cursor_index  > 0:
-      return values[crab_cursor_index - 1]
-    else:
-      return values[len(values) - 1]
-  
-  def move(self, num_move):
-    print("\n-- move {0}--".format(num_move))
-    print("cups: ", end="")
-    self.print_cups()
-    
-    pick_up = []
-    pick_cursor = self.crab_cursor
-    
-    for i in range(0,3):
-      pick_up.append(pick_cursor.next.value)
-      pick_cursor = pick_cursor.next
-
-    for i in range(0,3):
-      self.remove(pick_up[i])
-    
-    print("pick up: ", ' '.join([str(e) for e in pick_up]))
-    destination = self.destination(pick_up)
-    print("destination: ", destination)
-
-    # making the moves:
-    number_after = destination
-    for i in range(0,3):
-      self.add_after(number_after, pick_up[i])
-      number_after = pick_up[i]
-    self.crab_cursor = self.crab_cursor.next
-
-  
-  def destination2(self, pick_up):
-    values = []
-    cursor = self.head
-    values.append(cursor.value)
-    while cursor.next is not self.head:
-      cursor = cursor.next
-      values.append(cursor.value)
-    values.sort()
-    
-    crab_cursor_index = values.index(self.crab_cursor.value)
-    if crab_cursor_index  > 0:
-      return values[crab_cursor_index - 1]
-    else:
-      return values[len(values) - 1]
 
 
-  def move2(self, num_move):
-    
-    pick_up = []
-    pick_cursor = self.crab_cursor
-    
-    for i in range(0,3):
-      pick_up.append(pick_cursor.next.value)
-      pick_cursor = pick_cursor.next
-
-    for i in range(0,3):
-      self.remove(pick_up[i])
-    
-    destination = self.destination2(pick_up)
-
-    # making the moves:
-    number_after = destination
-    for i in range(0,3):
-      self.add_after(number_after, pick_up[i])
-      number_after = pick_up[i]
-    self.crab_cursor = self.crab_cursor.next
-
-
-  def add_after(self, num_after, num_to_add):
-    cursor = self.head
-    while cursor.value != num_after:
-      cursor = cursor.next
-    
-    newNode = Node(num_to_add)
-    
-    next_c = cursor.next
-    cursor.next = newNode
-    newNode.previous = cursor
-    newNode.next = next_c
-    next_c.previous = newNode
-    
   def play(self, n_moves):
+    
+    all_keys = self.nodes_map.keys()
+    min_num = min(all_keys)
+    max_num = max(all_keys)
+
     for i in range(0, n_moves):
-      self.move(i + 1)
-    print("\n-- final --")
-    print("cups: ", end="")
-    self.print_cups()
+      # self.move(i + 1)
+      # print(i + 1, end = "\r")
+
+      # print("\n-- move {0}--".format(i + 1))
+      # print("cups: ", end="")
+      # self.print_cups()
+      
+      pick_up = []
+      pick_cursor = self.crab_cursor.next
+      for i in range(0,3):      
+        pick_up.append(pick_cursor.value)
+        pick_cursor = pick_cursor.next
+      
+      for i in range(0,3):      
+        self.remove(pick_up[i])
+
+      destination = int(self.crab_cursor.value)
+
+      while True:
+        destination -= 1
+        if destination < min_num:
+          destination = max_num
+        if destination not in pick_up:
+          break 
+      
+      # print("pick up: ", ' '.join([str(e) for e in pick_up]))
+      # print("destination: ", destination)
+      
+      self.add_after(destination, pick_up)
+      self.crab_cursor = self.crab_cursor.next    # end for
+
+    # printing final results
+    # print("\n-- final --")
+    # print("cups: ", end="")
+    # self.print_cups()
 
   def add_str_array(self, str_array):
     for i in str_array:
-      self.add(i)
+      self.add(int(i))
   
   def labels_after_1(self):
     cursor = self.head
-    while cursor.value != str(1):
+    # while cursor.value != str(1):
+    while cursor.value != 1:
       cursor = cursor.next
     cursor = cursor.next
     res = []
-    while cursor.value != str(1):
+    while cursor.value != 1:
       res.append(cursor.value)
       cursor = cursor.next
 
-    print(res)
     return ''.join([str(e) for e in res])
 
-  play_2(self, n_moves):
 
     
 
@@ -200,22 +149,22 @@ class LinkedList:
 def part_1(input):
   linked_list = LinkedList()
   linked_list.add_str_array(input)
-
   linked_list.play(100)
-
   return linked_list.labels_after_1()
 
 def part_2(input):
   linked_list = LinkedList()
   linked_list.add_str_array(input)
-
-  linked_list.play(100)
-
-  return linked_list.labels_after_1()
+  for i in range(10, 1_000_001):
+    linked_list.add(i)
+  linked_list.play(10_000_000)
+  v1 = linked_list.nodes_map[1].next.value
+  v2 = linked_list.nodes_map[1].next.next.value
+  return v1 * v2
 
 def main():
-  print("Part 1:", part_1("643719258") )
-
+  print("Part 1:", part_1("643719258") )  
+  print("Part 2:", part_2("643719258") )
 
 
 if __name__ == "__main__":
